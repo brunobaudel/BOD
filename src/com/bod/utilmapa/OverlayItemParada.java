88150a2b0b3914871ab8f4d75e18ef70295aeba9
bruno baudel <brunobaudel@gmial.com>
2013-05-06 17:08:31 -0300

    commitando modificação

--- src/com/bod/utilmapa/OverlayItemParada.java
+++ src/com/bod/utilmapa/OverlayItemParada.java
@@ -1,22 +1,33 @@
 package com.bod.utilmapa;
 
-import android.view.View;
 
+import android.view.View;
+import android.widget.TextView;
+
+import com.bod.R;
 import com.bod.entidades.Parada;
 import com.google.android.maps.GeoPoint;
 
 
 public class OverlayItemParada extends OverlayItemOpenDocs{
 	
+	
+	Parada parada;
 
 	public OverlayItemParada(GeoPoint point, Parada parada) {
 		super(point, parada.getBairro(), parada.getCidade());
-		// TODO Auto-generated constructor stub
+		this.parada = parada;
 	}
 
 	@Override
 	public void setData(Object set, View v) {
 		v.setVisibility(View.VISIBLE);
+		
+		((TextView)v.findViewById(R.id.tvNumParada)).setText(parada.getCodigoParada());
+		((TextView)v.findViewById(R.id.tvBairro)).setText(parada.getBairro());
+		((TextView)v.findViewById(R.id.tvEnderecoParada)).setText(parada.getLogradouro());
+		
+		
 		
 	}
 	
